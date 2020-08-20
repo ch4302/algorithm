6e7d64c7ea19db564c7c98ad9e89d0aa9d74aaa1
CHAE\Chae <cjh4302@gmail.com>
2020-08-20 09:34:43 +0900

    2020.08.20

---
+++ Algospot/chapter_18/JOSEPHUS/josephus/josephus/source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+void josephus(int n, int k) {
+	list<int> survivors;
+	for (int i = 1; i <= n; i++) survivors.push_back(i);
+
+	list<int>::iterator kill = survivors.begin();
+	while (n > 2) {
+		kill = survivors.erase(kill);
+		if (kill == survivors.end()) kill = survivors.begin();
+		--n;
+
+		for (int i = 0; i < k - 1; i++) {
+			kill++;
+			if (kill == survivors.end()) kill = survivors.begin();
+		}
+	}
+
+	cout << survivors.front() << " " << survivors.back() << '\n';
+}
+
+int main() {
+	int n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		josephus(a, b);
+	}
+}
